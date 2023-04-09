b9ea9ce15ec5c31b58abb2d95d3131050f5fef5e
Adityachandii <76204985+Adityachandii@users.noreply.github.com>
2023-04-09 21:28:45 +0700

    first commit

---
+++ database/migrations/2023_04_09_131011_create_lecturers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_dosen');
+            $table->string('nama_mata_kuliah');
+            $table->unsignedBigInteger('kode_kelas');
+
+            $table->foreign('kode_kelas')->references('id')->on('kelas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturers');
+    }
+}
---
+++ database/migrations/2023_04_09_131106_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_mahasiswa');
+            $table->string('nama_mata_kuliah');
+            $table->unsignedBigInteger('kode_kelas');
+
+            $table->foreign('kode_kelas')->references('id')->on('kelas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
